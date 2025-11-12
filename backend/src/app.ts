4b913eeeeec171e804e42c75b373dd8edef30302
ViniciusDSDSouza <viniciusdossantos547@gmail.com>
2025-11-12 01:48:08 -0300

    security: add rate limiter and helmet

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,8 @@ import { partRoutes } from "./routes/partRoutes";
 import { enumNoteStatusRoutes } from "./routes/enumNoteStatusRoutes";
 import { noteRoutes } from "./routes/noteRoutes";
 import cookieParser from "cookie-parser";
+import { rateLimiter } from "./utils/rateLimiter";
+import helmet from "helmet";
 
 export const app = express();
 
@@ -22,6 +24,10 @@ app.use(
 app.use(cookieParser());
 
 app.use(express.json());
+
+app.use(rateLimiter);
+
+app.use(helmet());
 
 app.use("/", authRoutes);
 app.use("/customers", customerRoutes);
